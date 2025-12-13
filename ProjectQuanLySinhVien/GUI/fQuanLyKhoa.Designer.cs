0a2e0f0d296d006d38bcaf8eff3f3bfbe2eb3a77
Vương Nguyễn Đại <24522050@ms.uit.edu.vn>
2025-12-13 10:20:38 +0700

    Quản lý khoa

--- ProjectQuanLySinhVien/GUI/fQuanLyKhoa.Designer.cs
+++ ProjectQuanLySinhVien/GUI/fQuanLyKhoa.Designer.cs
@@ -16,27 +16,19 @@
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-
-            // --- Control nhập liệu KHOA ---
             this.txtTenKhoa = new System.Windows.Forms.TextBox();
             this.lbTenKhoa = new System.Windows.Forms.Label();
             this.txtMaKhoa = new System.Windows.Forms.TextBox();
             this.lbMaKhoa = new System.Windows.Forms.Label();
-
-            // --- Nút bấm ---
             this.btnLamMoi = new System.Windows.Forms.Button();
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
             this.btnThem = new System.Windows.Forms.Button();
-
-            // --- GridView ---
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.colMaKhoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colTenKhoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
-
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -44,7 +36,6 @@
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-
             // 
             // splitContainer1
             // 
@@ -54,7 +45,7 @@
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
-            // Panel 1 (Vùng nhập liệu)
+            // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.Color.White;
             this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
@@ -63,17 +54,17 @@
             this.splitContainer1.Panel1.Controls.Add(this.btnSua);
             this.splitContainer1.Panel1.Controls.Add(this.btnThem);
             // 
-            // Panel 2 (Vùng bảng)
+            // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
             this.splitContainer1.Size = new System.Drawing.Size(1000, 600);
-            this.splitContainer1.SplitterDistance = 200; // Giữ nguyên khoảng cách chuẩn
+            this.splitContainer1.SplitterDistance = 200;
             this.splitContainer1.TabIndex = 0;
-
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.txtTenKhoa);
             this.groupBox1.Controls.Add(this.lbTenKhoa);
             this.groupBox1.Controls.Add(this.txtMaKhoa);
@@ -86,115 +77,111 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "THÔNG TIN KHOA";
-
             // 
-            // Tên Khoa
+            // txtTenKhoa
+            // 
+            this.txtTenKhoa.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTenKhoa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTenKhoa.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtTenKhoa.Location = new System.Drawing.Point(390, 42);
+            this.txtTenKhoa.Name = "txtTenKhoa";
+            this.txtTenKhoa.Size = new System.Drawing.Size(550, 39);
+            this.txtTenKhoa.TabIndex = 2;
+            // 
+            // lbTenKhoa
             // 
             this.lbTenKhoa.AutoSize = true;
             this.lbTenKhoa.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.lbTenKhoa.Location = new System.Drawing.Point(300, 45);
             this.lbTenKhoa.Name = "lbTenKhoa";
-            this.lbTenKhoa.Size = new System.Drawing.Size(83, 23);
+            this.lbTenKhoa.Size = new System.Drawing.Size(116, 32);
             this.lbTenKhoa.TabIndex = 2;
             this.lbTenKhoa.Text = "Tên khoa:";
-
-            this.txtTenKhoa.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtTenKhoa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtTenKhoa.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtTenKhoa.Location = new System.Drawing.Point(390, 42); // Đẩy ra xa một chút cho đẹp
-            this.txtTenKhoa.Name = "txtTenKhoa";
-            this.txtTenKhoa.Size = new System.Drawing.Size(550, 30); // Rộng hơn vì không còn ô tín chỉ
-            this.txtTenKhoa.TabIndex = 2;
-
             // 
-            // Mã Khoa
+            // txtMaKhoa
+            // 
+            this.txtMaKhoa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMaKhoa.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtMaKhoa.Location = new System.Drawing.Point(110, 42);
+            this.txtMaKhoa.Name = "txtMaKhoa";
+            this.txtMaKhoa.Size = new System.Drawing.Size(150, 39);
+            this.txtMaKhoa.TabIndex = 1;
+            // 
+            // lbMaKhoa
             // 
             this.lbMaKhoa.AutoSize = true;
             this.lbMaKhoa.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.lbMaKhoa.Location = new System.Drawing.Point(30, 45);
             this.lbMaKhoa.Name = "lbMaKhoa";
-            this.lbMaKhoa.Size = new System.Drawing.Size(81, 23);
+            this.lbMaKhoa.Size = new System.Drawing.Size(112, 32);
             this.lbMaKhoa.TabIndex = 0;
             this.lbMaKhoa.Text = "Mã khoa:";
-
-            this.txtMaKhoa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtMaKhoa.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtMaKhoa.Location = new System.Drawing.Point(110, 42);
-            this.txtMaKhoa.Name = "txtMaKhoa";
-            this.txtMaKhoa.Size = new System.Drawing.Size(150, 30);
-            this.txtMaKhoa.TabIndex = 1;
-
+            //this.lbMaKhoa.Click += new System.EventHandler(this.lbMaKhoa_Click);
             // 
-            // CÁC NÚT BẤM (Giữ nguyên vị trí chuẩn)
-            // 
-            int btnY = 145;
-
             // btnLamMoi
+            // 
             this.btnLamMoi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnLamMoi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
             this.btnLamMoi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLamMoi.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnLamMoi.ForeColor = System.Drawing.Color.White;
-            this.btnLamMoi.Location = new System.Drawing.Point(860, btnY);
+            this.btnLamMoi.Location = new System.Drawing.Point(860, 145);
             this.btnLamMoi.Name = "btnLamMoi";
             this.btnLamMoi.Size = new System.Drawing.Size(120, 39);
             this.btnLamMoi.TabIndex = 7;
             this.btnLamMoi.Text = "LÀM MỚI";
             this.btnLamMoi.UseVisualStyleBackColor = false;
-            //this.btnLamMoi.Click += new System.EventHandler(this.btnLamMoi_Click);
-
+            // 
             // btnXoa
+            // 
             this.btnXoa.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnXoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
             this.btnXoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnXoa.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnXoa.ForeColor = System.Drawing.Color.White;
-            this.btnXoa.Location = new System.Drawing.Point(599, btnY);
+            this.btnXoa.Location = new System.Drawing.Point(599, 145);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(116, 39);
             this.btnXoa.TabIndex = 6;
             this.btnXoa.Text = "XÓA";
             this.btnXoa.UseVisualStyleBackColor = false;
-            //this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
-
+            // 
             // btnSua
+            // 
             this.btnSua.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(156)))), ((int)(((byte)(18)))));
             this.btnSua.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSua.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnSua.ForeColor = System.Drawing.Color.White;
-            this.btnSua.Location = new System.Drawing.Point(732, btnY);
+            this.btnSua.Location = new System.Drawing.Point(732, 145);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(113, 39);
             this.btnSua.TabIndex = 5;
             this.btnSua.Text = "SỬA";
             this.btnSua.UseVisualStyleBackColor = false;
-            //this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
-
+            // 
             // btnThem
+            // 
             this.btnThem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnThem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.btnThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnThem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnThem.ForeColor = System.Drawing.Color.White;
-            this.btnThem.Location = new System.Drawing.Point(470, btnY);
+            this.btnThem.Location = new System.Drawing.Point(470, 145);
             this.btnThem.Name = "btnThem";
             this.btnThem.Size = new System.Drawing.Size(113, 39);
             this.btnThem.TabIndex = 4;
             this.btnThem.Text = "THÊM";
             this.btnThem.UseVisualStyleBackColor = false;
-            //this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
-
             // 
-            // DATAGRIDVIEW
+            // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-
-            // Header Style
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 10F);
@@ -204,14 +191,9 @@
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeight = 45;
-            this.dataGridView1.EnableHeadersVisualStyles = false;
-
-            // Columns
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colMaKhoa,
             this.colTenKhoa});
-
-            // Cell Style
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10F);
@@ -220,8 +202,8 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
             this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.MultiSelect = false;
@@ -233,21 +215,22 @@
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(1000, 396);
             this.dataGridView1.TabIndex = 0;
-            //this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
-
-            // Cột 1
+            // 
+            // colMaKhoa
+            // 
             this.colMaKhoa.DataPropertyName = "MaKhoa";
             this.colMaKhoa.HeaderText = "MÃ KHOA";
+            this.colMaKhoa.MinimumWidth = 9;
             this.colMaKhoa.Name = "colMaKhoa";
             this.colMaKhoa.ReadOnly = true;
-            this.colMaKhoa.Width = 200;
-
-            // Cột 2
+            // 
+            // colTenKhoa
+            // 
             this.colTenKhoa.DataPropertyName = "TenKhoa";
             this.colTenKhoa.HeaderText = "TÊN KHOA";
+            this.colTenKhoa.MinimumWidth = 9;
             this.colTenKhoa.Name = "colTenKhoa";
             this.colTenKhoa.ReadOnly = true;
-
             // 
             // fQuanLyKhoa
             // 
@@ -259,7 +242,6 @@
             this.Name = "fQuanLyKhoa";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý khoa";
-
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -268,6 +250,7 @@
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+
         }
 
         #endregion
--- ProjectQuanLySinhVien/GUI/fQuanLyKhoa.cs
+++ ProjectQuanLySinhVien/GUI/fQuanLyKhoa.cs
@@ -1,25 +1,186 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace ProjectQuanLySinhVien.GUI
 {
     public partial class fQuanLyKhoa : Form
     {
+        
+        private string strKetNoi = @"Data Source=DESKTOP-E2VL8VG\SQLEXPRESS;Initial Catalog=QLSV_DB;Integrated Security=True;Connect Timeout=3";
+        SqlConnection conn = null;
+
         public fQuanLyKhoa()
         {
             InitializeComponent();
+            LoadData();
+            GanSuKien(); 
         }
 
-        private void fQuanLyKhoa_Load(object sender, EventArgs e)
+        
+        private void GanSuKien()
         {
+            btnThem.Click += new EventHandler(btnThem_Click);
+            btnSua.Click += new EventHandler(btnSua_Click);
+            btnXoa.Click += new EventHandler(btnXoa_Click);
+            btnLamMoi.Click += new EventHandler(btnLamMoi_Click);
+            dataGridView1.CellClick += new DataGridViewCellEventHandler(dataGridView1_CellClick);
+        }
 
+        private void LoadData()
+        {
+            try
+            {
+                if (conn == null) conn = new SqlConnection(strKetNoi);
+
+                
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM KHOA", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show("Lỗi khi tải dữ liệu Khoa: " + ex.Message, "Lỗi kết nối", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Kiểm tra mã khoa đã tồn tại chưa
+        private bool KiemTraMaKhoaTonTai(string maKhoa)
+        {
+            if (string.IsNullOrWhiteSpace(maKhoa)) return false;
+            try
+            {
+                using (var localConn = new SqlConnection(strKetNoi))
+                using (var cmd = new SqlCommand("SELECT COUNT(*) FROM KHOA WHERE MaKhoa=@ma", localConn))
+                {
+                    cmd.Parameters.AddWithValue("@ma", maKhoa.Trim());
+                    localConn.Open();
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return count > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Nếu không thể kiểm tra, báo lỗi và trả về true để ngăn insert trùng khóa do lỗi kết nối
+                MessageBox.Show("Không thể kiểm tra tồn tại Mã Khoa. Vui lòng kiểm tra kết nối.\n" + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return true;
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtMaKhoa.Text = row.Cells["colMaKhoa"].Value?.ToString() ?? string.Empty;
+                txtTenKhoa.Text = row.Cells["colTenKhoa"].Value?.ToString() ?? string.Empty;
+                txtMaKhoa.Enabled = false;
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (txtMaKhoa.Text == "") { MessageBox.Show("Vui lòng nhập Mã Khoa!"); return; }
+
+            string ma = txtMaKhoa.Text.Trim();
+            if (KiemTraMaKhoaTonTai(ma))
+            {
+                MessageBox.Show("Mã khoa đã tồn tại. Vui lòng nhập mã khác.", "Trùng mã", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaKhoa.Focus();
+                return;
+            }
+
+            try
+            {
+                if (conn == null) conn = new SqlConnection(strKetNoi);
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                string sql = "INSERT INTO KHOA (MaKhoa, TenKhoa) VALUES (@ma, @ten)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ma", ma);
+                cmd.Parameters.AddWithValue("@ten", txtTenKhoa.Text.Trim());
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Thêm thành công!");
+                LoadData();
+                ResetForm();
+            }
+            catch (SqlException ex)
+            {
+                // Sql error codes 2627 and 2601 indicate unique constraint / PK violation
+                if (ex.Number == 2627 || ex.Number == 2601)
+                {
+                    MessageBox.Show("Không thể thêm: Mã khoa đã tồn tại (khóa chính trùng). Vui lòng nhập mã khác.", "Lỗi trùng khóa", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Lỗi SQL: " + ex.Message);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Lỗi: " + ex.Message); }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open) conn.Close();
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn == null) conn = new SqlConnection(strKetNoi);
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                string sql = "UPDATE KHOA SET TenKhoa=@ten WHERE MaKhoa=@ma";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ma", txtMaKhoa.Text.Trim());
+                cmd.Parameters.AddWithValue("@ten", txtTenKhoa.Text.Trim());
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Sửa thành công!");
+                LoadData();
+                ResetForm();
+            }
+            catch (Exception ex) { MessageBox.Show("Lỗi: " + ex.Message); }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open) conn.Close();
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xóa khoa này?", "Xác nhận", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    if (conn == null) conn = new SqlConnection(strKetNoi);
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM KHOA WHERE MaKhoa=@ma", conn);
+                    cmd.Parameters.AddWithValue("@ma", txtMaKhoa.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Xóa thành công!");
+                    LoadData();
+                    ResetForm();
+                }
+                catch (Exception ex) { MessageBox.Show("Không thể xóa: " + ex.Message); }
+                finally
+                {
+                    if (conn != null && conn.State == ConnectionState.Open) conn.Close();
+                }
+            }
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        private void ResetForm()
+        {
+            txtMaKhoa.Text = "";
+            txtTenKhoa.Text = "";
+            txtMaKhoa.Enabled = true;
+            txtMaKhoa.Focus();
         }
     }
 }
